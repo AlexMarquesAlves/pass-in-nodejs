6584b62d22ef26d50602dc634c1bf35b5603ccd6
¡Duque <atanaelleonardo@gmail.com>
2024-04-04 23:01:36 -0300

    feat: "Remove unused route handlers, update route for events POST request, and update log message"
    Explanation:
    
    * The code diff shows that the `/` and `/users` route handlers have been removed.
    * A new route handler for `/events` using the POST method has been added.
    * The log message in the `app.listen` callback has been updated to use `console.log` instead of `log`.

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,13 @@
 import "dotenv/config";
-import { log } from "console";
 import fastify from "fastify";
 
 const app = fastify();
 const port = process.env.PORT || 3333;
 
-app.get("/", async (req, res) => {
-  return "Hello NLW Unite!";
-});
-
-app.get("/users", async (req, res) => {
-  return "Hello teste!";
-});
-app.post("/users", async (req, res) => {
+app.post("/events", async (req, res) => {
   return "Hello teste!";
 });
 
 app.listen({ port }).then(() => {
-  log(`🚀 HTTP server  is listening on port ${port}!`);
+  console.log(`🚀 HTTP server  is listening on port ${port}!`);
 });
