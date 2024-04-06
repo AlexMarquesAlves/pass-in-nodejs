ba102591a29693b54781a883ce15bb988e4ed088
¡Duque <atanaelleonardo@gmail.com>
2024-04-05 21:34:57 -0300

    feat: "Move schema validation into separate variable and destructure request body"
    This commit moves the schema validation for the request body of the `/events` POST endpoint into a separate variable, and then destructures the request body from it. This makes the code more modular and easier to read. Additionally, the error handling for an event with the same name has been moved into the main request handler.

--- src/server.ts
+++ src/server.ts
@@ -18,34 +18,46 @@ const prisma = new PrismaClient({
   log: ["query"],
 });
 
-app.withTypeProvider<ZodTypeProvider>().post("/events", async (req, res) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  });
-
-  const { title, details, maximumAttendees } = createEventSchema.parse(
-    req.body
-  );
-  const slug = generateSlug(title);
-  const eventWithSameSlug = await prisma.event.findUnique({ where: { slug } });
-
-  if (eventWithSameSlug !== null) {
-    throw new Error("An event with the same name already exists");
-  }
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximumAttendees,
-      slug,
+app.withTypeProvider<ZodTypeProvider>().post(
+  "/events",
+  {
+    schema: {
+      body: z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+      }),
     },
-  });
+  },
+  async (req, res) => {
+    // const createEventSchema = z.object({
+    //   title: z.string().min(4),
+    //   details: z.string().nullable(),
+    //   maximumAttendees: z.number().int().positive().nullable(),
+    // });
 
-  return res.status(201).send({ event: event.id });
-});
+    const { title, details, maximumAttendees } = req.body;
+    const slug = generateSlug(title);
+    const eventWithSameSlug = await prisma.event.findUnique({
+      where: { slug },
+    });
+
+    if (eventWithSameSlug !== null) {
+      throw new Error("An event with the same name already exists");
+    }
+
+    const event = await prisma.event.create({
+      data: {
+        title,
+        details,
+        maximumAttendees,
+        slug,
+      },
+    });
+
+    return res.status(201).send({ event: event.id });
+  }
+);
 
 app.listen({ port }).then(() => {
   console.log(`🚀 HTTP server  is listening on port ${port}!`);
