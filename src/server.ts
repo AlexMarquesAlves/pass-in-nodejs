3abb2b9e969f6cd877e438fa8634e6b8074730fb
¡Duque <atanaelleonardo@gmail.com>
2024-04-06 02:30:38 -0300

    feat: "Add CORS support to server"
    This commit adds support for Cross-Origin Resource Sharing (CORS) to the server by registering the `@fastify/cors` plugin. The `origin` is set to `*`, allowing all origins to access the server's resources.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
 import {
   ZodTypeProvider,
@@ -18,6 +19,10 @@ import { registerForEvent } from "./routes/register-for-event";
 import { errorHandler } from "./error-handler";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(fastifySwagger, {
   swagger: {
