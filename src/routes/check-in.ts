9d3acf31ab40f87c0c09ab010fad794984bc33ff
¡Duque <atanaelleonardo@gmail.com>
2024-04-06 00:09:06 -0300

    feat: "Add Zod schema for check-in route params and response"
    This commit adds a Zod schema for the `params` and `response` of the `checkIn` route. The schema for the `params` specifies that the `attendeeId` must be a coerced number and an integer. The `response` schema is an empty object.

--- src/routes/check-in.ts
+++ src/routes/check-in.ts
@@ -1,8 +1,18 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
 
 function checkIn(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .get("/attendees/:attendeeId/check-in", (response, reply) => {});
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/attendees/:attendeeId/check-in",
+    {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int(),
+        }),
+        response: {},
+      },
+    },
+    (response, reply) => {}
+  );
 }
