c3d5584ecc50804e0870e95ed454c9b1fa51bc00
¡Duque <atanaelleonardo@gmail.com>
2024-04-05 22:35:44 -0300

    feat: "Add Zod schema validation for event ID in get-event route"
    This commit adds Zod schema validation for the `eventId` parameter in the `get-event` route. The `eventId` is now validated to be a universally unique identifier (UUID) before handling the request.

--- src/routes/get-event.ts
+++ src/routes/get-event.ts
@@ -3,7 +3,15 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 
 export async function getEvent(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .get("/events/:eventId", async (request, reply) => {});
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/events/:eventId",
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request, reply) => {}
+  );
 }
