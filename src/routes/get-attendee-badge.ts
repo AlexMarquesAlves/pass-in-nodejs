3ae302ce304100e0c03308ad689cbada86d1f232
¡Duque <atanaelleonardo@gmail.com>
2024-04-05 23:52:42 -0300

    feat: "Update getAttendeeBadge schema to include badge object in 200 response"

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -13,7 +13,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         params: z.object({
           attendeeId: z.coerce.number().int(),
         }),
-        response: {},
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
