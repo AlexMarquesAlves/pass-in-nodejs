9420122d6793f97a8959c5339cffc784db629a1e
¡Duque <atanaelleonardo@gmail.com>
2024-04-05 21:46:49 -0300

    feat: Remove event creation endpoint and Prisma client setup
    This commit removes the event creation endpoint and the setup of the Prisma client. The endpoint code and the Prisma client instantiation have been commented out. The server still listens on port 3333.

--- src/server.ts
+++ src/server.ts
@@ -14,54 +14,6 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 const port = 3333;
-const prisma = new PrismaClient({
-  log: ["query"],
-});
-
-app.withTypeProvider<ZodTypeProvider>().post(
-  "/events",
-  {
-    schema: {
-      body: z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-      }),
-      response: {
-        201: z.object({ eventId: z.string().uuid() }),
-      },
-    },
-  },
-  async (req, res) => {
-    // const createEventSchema = z.object({
-    //   title: z.string().min(4),
-    //   details: z.string().nullable(),
-    //   maximumAttendees: z.number().int().positive().nullable(),
-    // });
-
-    const { title, details, maximumAttendees } = req.body;
-    const slug = generateSlug(title);
-    const eventWithSameSlug = await prisma.event.findUnique({
-      where: { slug },
-    });
-
-    if (eventWithSameSlug !== null) {
-      throw new Error("An event with the same name already exists");
-    }
-
-    const event = await prisma.event.create({
-      data: {
-        title,
-        details,
-        maximumAttendees,
-        slug,
-      },
-    });
-
-    return res.status(201).send({ eventId: event.id });
-  }
-);
-
 app.listen({ port }).then(() => {
   console.log(`🚀 HTTP server  is listening on port ${port}!`);
 });
